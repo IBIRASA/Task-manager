27d55de7fdf57cbcf289dcce46834930694c0801
IBIRASA <d.ishimwe@alustudent.com>
2024-10-30 15:17:33 +0200

    sidebar

--- task-manager/src/Components/Sidebar.jsx
+++ task-manager/src/Components/Sidebar.jsx
@@ -1,12 +1,13 @@
-// Sidebar.tsx
 import { Link } from 'react-router-dom';
-import { FaHome, FaTasks, FaUserFriends, FaEnvelope, FaCog } from 'react-icons/fa';
+import { FaHome, FaTasks, FaUserFriends, FaCog } from 'react-icons/fa';
+import { FaBookOpen, FaEnvelope } from 'react-icons/fa6'; // Correct icons
+
 function Sidebar() {
   return (
     <div className="w-64 h-screen bg-gray-100 text-white flex flex-col p-4">
       <div className="text-xl font-bold mb-8 flex items-center space-x-2">
-        <div className="bg-purple-600 w-8 h-8 rounded-full flex items-center justify-center">
-          
+        <div className="bg-purple-600 w-8 h-8 rounded-md flex items-center justify-center">
+        <FaBookOpen className=" text-gray-100" />
         </div>
         <span className='text-gray-900'>Nuegas</span>
       </div>
@@ -15,7 +16,7 @@ function Sidebar() {
           <FaHome className="mr-3 text-gray-700" /> <span className='text-gray-700'> Overview</span> 
         </Link>
         <Link to="/task" className="flex items-center p-2 rounded hover:bg-gray-700 transition">
-          <FaTasks className="mr-3 text-gray-700" />  <span className='text-gray-700'>Task</span> 
+          <FaBookOpen className="mr-3 text-gray-700" />  <span className='text-gray-700'>Task</span> 
         </Link>
         <Link to="/mentors" className="flex items-center p-2 rounded hover:bg-gray-700 transition">
           <FaUserFriends className="mr-3 text-gray-700" />  <span className='text-gray-700'> Mentors</span> 
